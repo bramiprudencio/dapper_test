e2fe0613ec3a6e5ae8ce33c2e0cc8798858a79c5
bramiprudencio <bramiprudencio@gmail.com>
2025-12-22 17:43:47 -0400

    colombia data on postgres

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import pandas as pd
+from openpyxl import load_workbook
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, Text, DateTime
+from sqlalchemy.orm import sessionmaker, declarative_base
+from google import genai
 
 url_proyectos_colombia  = "https://www.camara.gov.co/proyectos-de-ley/#menu"
 
@@ -17,6 +21,14 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
+
+DATABASE_URL = "postgresql://admin:Pass123@localhost:5432/legislacion_db"
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+metadata = MetaData()
+
 '''
 response = requests.post(php_url, data=php_payload, headers=headers)
 if response.status_code == 200:
@@ -25,5 +37,56 @@ if response.status_code == 200:
     print("Archivo descargado exitosamente.")
 '''
 
-proyectos_de_ley_colombia = pd.read_excel('proyectos_de_ley_colombia.xlsx')
-print(proyectos_de_ley_colombia.head())
+wb = load_workbook('proyectos_de_ley_colombia.xlsx').active
+df = pd.read_excel('proyectos_de_ley_colombia.xlsx')
+
+df['Fecha Cámara'] = pd.to_datetime(df['Fecha Cámara'], errors='coerce')
+
+links = []
+
+for cell in wb['P'][1:]:
+
+    if cell.hyperlink:
+        links.append(cell.hyperlink.target)
+    else:
+        links.append(None)
+df['Link'] = links if len(links) == len(df) else None
+print(df.head())
+
+class ProyectoLey(Base):
+    __tablename__ = 'proyectos_ley'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    titulo = Column(String)
+    fecha_radicacion = Column(DateTime)
+    resumen = Column(Text)
+    enlace = Column(String)
+    estado = Column(String)
+    pais = Column(String)
+    sector_economico = Column(String)
+
+Base.metadata.create_all(engine)
+
+for index, row in df.iterrows():
+    titulo = row['Título'] if not pd.isna(row['Título']) else None
+    fecha_radicacion = row['Fecha Cámara'] if not pd.isna(row['Fecha Cámara']) else None
+    resumen = row['Objeto del proyecto'] if not pd.isna(row['Objeto del proyecto']) else None
+    estado = row['Estado de Ley'] if not pd.isna(row['Estado de Ley']) else None
+
+    proyecto = ProyectoLey(
+        titulo=titulo,
+        fecha_radicacion=fecha_radicacion,
+        resumen=resumen,
+        enlace=row['Link'],
+        estado=estado,
+        pais='Colombia',
+        sector_economico='N/A'
+    )
+    session.add(proyecto)
+try:
+    session.commit()
+    print("✅ Datos insertados exitosamente.")
+except Exception as e:
+    session.rollback()
+    print(f"❌ Error al guardar en BD: {e}")
+finally:
+    session.close()
